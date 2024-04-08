3a66869aaf792a5cddb51c4cc8b119d27b5e3676
James Rogan <jamesrogan53@gmail.com>
2024-04-08 12:07:02 -0400

    WORKING: Radix 2 Multiplier

---
+++ Working_local/Booth_Mult.vhd
@@ -0,0 +1,74 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Booth_Mult is
+    Port (
+        In_1, In_2 : in std_logic_vector (7 downto 0);
+        clk : in std_logic;
+        ready : in std_logic;
+        done : out std_logic;
+        S : out std_logic_vector (15 downto 0)
+    );
+end Booth_Mult;
+
+architecture arch of Booth_Mult is
+
+    signal M : std_logic_vector (7 downto 0);
+    signal Q : std_logic_vector (8 downto 0);
+    signal A : std_logic_vector (7 downto 0);
+    signal cond : std_logic_vector(1 downto 0);
+    signal AQ : std_logic_vector(16 downto 0);
+    signal i : integer := 0;
+    signal step : integer := 0;
+
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            case step is
+                when 0 =>
+                    if ready = '1' then
+                        M <= (In_1); 
+                        Q <= In_2 & '0';
+                        A <= (others => '0'); -- Initialize A to 0
+                        step <= step + 1;
+                        done <= '0';
+                        S <= (15 downto 0 => '0');
+                    end if;
+                when 1 =>
+                        cond <= Q(1 downto 0);
+                        step <= step + 1;
+                when 2 =>
+                    case cond is
+                        when "10" =>
+                            A <= std_logic_vector(signed(A) - signed(M)); -- this used to be unsigned but i think it should be signed
+                        when "01" =>
+                            A <= std_logic_vector(signed(A) + signed(M)); -- same here
+                        when others =>
+                            null;
+                    end case;
+                    step <= step + 1;
+                when 3 =>
+                    AQ <= std_logic_vector(shift_right(signed(A & Q),1)); -- shifting of AQ happens each time
+                    step <= step + 1;
+                when 4 =>
+                    Q <= AQ(8 downto 0); -- split up A and Q
+                    A <= AQ(16 downto 9);
+                    i <= i + 1; -- increment i by 1
+                    if (i = 7) then
+                        step <= step + 1;
+                        i <= 0;
+                    else
+                        step <= 1;
+                    end if;
+                when 5 =>
+                    step <= 0;
+                    done <= '1';  
+                    S <= A & Q(8 downto 1);
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process;
+end arch;
---
+++ Working_local/Booth_Mult_tb.vhd
@@ -0,0 +1,86 @@
+library IEEE ;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Booth_Mult_tb is
+end Booth_Mult_tb;
+
+architecture arch of Booth_Mult_tb is
+    signal In_1_tb : std_logic_vector (7 downto 0);
+    signal In_2_tb : std_logic_vector (7 downto 0);
+    signal clk_tb : std_logic := '0';
+    signal ready_tb : std_logic := '0';
+    signal done_tb : std_logic;
+    signal S_tb : std_logic_vector (15 downto 0);
+
+    component Booth_Mult
+        port  (
+            In_1, In_2 : in std_logic_vector (7 downto 0) ;
+            clk : in std_logic ;
+            ready : in std_logic;
+            done : out std_logic;
+            S : out std_logic_vector (15 downto 0)
+        );
+    end component;
+
+    constant clk_period : time := 20 ns; -- Clock period
+
+begin
+
+    -- Clock generation process
+    clk_gen_proc: process
+    begin
+        wait for 10 ns;
+        while now < 5 us loop -- Simulate for 1000 ns
+            clk_tb <= not clk_tb; -- Toggle clock every half period
+            wait for clk_period / 2;
+        end loop;
+        wait;
+    end process;
+
+    -- Stimulus process
+    stimulus: process
+    begin
+        -- Test case 1: Expected out 00010101 (21)
+        In_1_tb <= "00000111";  -- 7
+        In_2_tb <= "00000011";  -- 3
+        ready_tb <= '1';
+        wait until rising_edge(clk_tb);
+        ready_tb <= '0'; -- load the M and Q registers with the inputs
+        wait until done_tb = '1';
+        wait for 10 ns;
+
+        -- Test case 2: Expected out 00010101 (-21)
+        In_1_tb <= "00000111";  -- 7
+        In_2_tb <= "11111101";  -- -3
+        ready_tb <= '1';
+        wait until rising_edge(clk_tb);
+        ready_tb <= '0'; -- algorithm begins
+        wait until done_tb = '1'; -- signals S register is updated
+        wait for 10 ns;
+
+        -- Test case 3: Expected out 10010111 (-105)
+        In_1_tb <= "00100011";  -- 35
+        In_2_tb <= "11111101";  -- -3
+        ready_tb <= '1';
+        wait for 10 ns;
+        wait until rising_edge(clk_tb);
+        ready_tb <= '0'; -- algorithm begins
+        wait until done_tb = '1'; -- signals S register is updated
+        wait for 10 ns;
+
+        wait; -- until end
+    end process stimulus;
+
+    -- Instantiate the DUT
+    UUT: Booth_Mult
+        port map (
+            In_1 => In_1_tb,
+            In_2 => In_2_tb,
+            clk => clk_tb,
+            ready => ready_tb,
+            done => done_tb,
+            S => S_tb
+        );
+
+end arch;
