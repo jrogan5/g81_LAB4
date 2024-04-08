62830c0799e37b3d7a5f3f6070d4da8237559a8b
hamzadaqa <abudaqahamza@gmail.com>
2024-04-07 23:16:59 -0400

    Main change is on line 46 AND 48 AND 57. I think these changes should make the code work, simulate it and lmk pls. DID NOT TEST YET

--- Booth_Mult.vhd
+++ Booth_Mult.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity Booth_Mult is
-    Port ( 
+    Port (
         In_1, In_2 : in std_logic_vector (7 downto 0);
         clk : in std_logic;
         ready : in std_logic;
@@ -12,9 +12,9 @@ entity Booth_Mult is
     );
 end Booth_Mult;
 
-architecture arch of Booth_Mult is 
+architecture arch of Booth_Mult is
 
-    signal M : signed (7 downto 0);
+    signal M : std_logic_vector (7 downto 0);
     signal Q : std_logic_vector (8 downto 0);
     signal A : std_logic_vector (7 downto 0);
     signal cond : std_logic_vector(1 downto 0);
@@ -24,50 +24,53 @@ architecture arch of Booth_Mult is
     signal i : integer := 0;
     signal step : integer := 0;
 
-begin 
+begin
     process(clk)
     begin
-        case step is 
-                when 0 => 
-                if ready = '1' then
-                    M <= signed(In_1); -- converts M to 2's complement 
-                    Q <= In_2 & '0';
-                    A <= (7 downto 0 => '0'); -- Initialize A to 0
-                    i <= 0; -- initialize index to 0
+        if rising_edge(clk) then
+            case step is
+                when 0 =>
+                    if ready = '1' then
+                        M <= (In_1); -- converts M to 2's complement
+                        Q <= In_2 & '0';
+                        A <= (others => '0'); -- Initialize A to 0
+                        A_int <= (others => '0');
+                        step <= step + 1;
+                        done <= '0';
+                        S <= (others => '0');
+                    end if;
+                when 1 =>
+                    cond <= Q(1 downto 0);
+                    case cond is
+                        when "10" =>
+                            A <= std_logic_vector(signed(A_int) - signed(M)); -- this used to be unsigned but i think it should be signed
+                        when "01" =>
+                            A <= std_logic_vector(signed(A_int) + signed(M)); -- same here
+                        when others =>
+                            null;
+                    end case;
                     step <= step + 1;
-                    done <= '0';
-                end if;
-            when 1 =>
-                cond <= std_logic_vector(Q(1 downto 0));
-                case cond is
-                    when "10" =>
-                        A <= A_int - M;
-                    when "01" =>
-                        A <= A_int + M;
-                    when others => 
-                end case;
-                step <= step + 1;
-            when 2 =>    
-                AQ <= std_logic_vector(A) & std_logic_vector(Q); -- shifting of AQ happens each time
-                step <= step + 1;
-            when 3 =>
-                AQ_shifted <= std_logic_vector(shift_right(unsigned(AQ),1));
-                step <= step + 1;
-            when 4 =>
-                Q <= signed(AQ_shifted(8 downto 0)); -- split up A and Q 
-                A <= signed(AQ_shifted(16 downto 9));
-                A_int <= signed(AQ_shifted(16 downto 9));
-                i <= i + 1; -- increment i by 1
-                if (i = 8) then
-                    step <= 0;
-                else 
-                    step <= 1;
-                end if;
-            when others => 
-        end case;  
+                when 2 =>
+                    AQ <= A & Q; -- shifting of AQ happens each time
+                    step <= step + 1;
+                when 3 =>
+                    AQ_shifted <= AQ(0) & AQ(16 downto 1); -- used to be '0' & AQ(16 downto 1)
+                    step <= step + 1;
+                when 4 =>
+                    Q <= AQ_shifted(8 downto 0); -- split up A and Q
+                    A <= AQ_shifted(16 downto 9);
+                    A_int <= AQ_shifted(16 downto 9);
+                    i <= i + 1; -- increment i by 1
+                    if (i = 8) then
+                        step <= 0;
+                        done <= '1';
+                        S <= AQ_shifted(16 downto 1);
+                    else
+                        step <= 1;
+                    end if;
+                when others =>
+                    null;
+            end case;
+        end if;
     end process;
-
-    done <= '1' when (i = 8) else '0';
-    S <= AQ_shifted(16 downto 1) when (i = 8) else (15 downto 0 => '0');
-
 end arch;
--- Booth_Mult_tb.vhd
+++ Booth_Mult_tb.vhd
@@ -2,10 +2,10 @@ library IEEE ;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity Booth_Mult_tb is 
+entity Booth_Mult_tb is
 end Booth_Mult_tb;
 
-architecture arch of Booth_Mult_tb is 
+architecture arch of Booth_Mult_tb is
     signal In_1_tb : std_logic_vector (7 downto 0);
     signal In_2_tb : std_logic_vector (7 downto 0);
     signal clk_tb : std_logic := '0';
@@ -14,12 +14,12 @@ architecture arch of Booth_Mult_tb is
     signal S_tb : std_logic_vector (15 downto 0);
 
     component Booth_Mult
-        port  ( 
+        port  (
             In_1, In_2 : in std_logic_vector (7 downto 0) ;
             clk : in std_logic ;
             ready : in std_logic;
             done : out std_logic;
-            S : out std_logic_vector (15 downto 0) 
+            S : out std_logic_vector (15 downto 0)
         );
     end component;
 
@@ -41,12 +41,12 @@ begin
     stimulus: process
     begin
         -- algoritthm:
-        -- 1st clock: ready step. loads them in. 
-        -- 2nd: 
+        -- 1st clock: ready step. loads them in.
+        -- 2nd:
         In_1_tb <= "11111001";  -- -7
         In_2_tb <= "11111101";  -- -3
         ready_tb <= '1';
-	    wait for 10 ns;
+        wait for 10 ns;
         wait until rising_edge(clk_tb);
         ready_tb <= '0'; -- Start the multiplication process
 
@@ -54,20 +54,20 @@ begin
         wait until done_tb = '1';
 
         -- Add any additional checks or assertions here
-        
+
         -- End simulation
         wait;
     end process stimulus;
 
     -- Instantiate the DUT
     UUT: Booth_Mult
-    port map (
-        In_1 => In_1_tb,
-        In_2 => In_2_tb,
-        clk => clk_tb,
-        ready => ready_tb,
-        done => done_tb,
-        S => S_tb
-    );
+        port map (
+            In_1 => In_1_tb,
+            In_2 => In_2_tb,
+            clk => clk_tb,
+            ready => ready_tb,
+            done => done_tb,
+            S => S_tb
+        );
 
 end arch;
